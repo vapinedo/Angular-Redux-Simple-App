2bfdb748f44599a2dc91afac0989442e64d42538
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-08-11 18:14:30 -0500

    [add] storeDevTools via npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { counterReducer } from './counter/counter.reducer';
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './counter/child/child.component';
@@ -17,6 +19,11 @@ import { GrandChildComponent } from './counter/grand-child/grand-child.component
   imports: [
     BrowserModule,
     StoreModule.forRoot({ counter: counterReducer }), 
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
